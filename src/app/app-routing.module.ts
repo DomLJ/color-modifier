71229ab9f756a01e43e828a38d3f74547a4d81a2
DomLJ <dominik34@o2.pl>
2020-07-19 21:11:29 +0200

    Create color modification and gradient modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ColorCreatorComponent } from './color-creator/color-creator.component';
+import { GradientCreatorComponent } from './gradient-creator/gradient-creator.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ColorCreatorComponent },
+  { path: 'gradients', component: GradientCreatorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ColorCreatorComponent } from './color-creator/color-creator.component';
+import { FooterComponent } from './footer/footer.component';
+import { RgbSliderComponent } from './rgb-slider/rgb-slider.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomHueComponent } from './custom-hue/custom-hue.component';
+import { GradientCreatorComponent } from './gradient-creator/gradient-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ColorCreatorComponent,
+    FooterComponent,
+    RgbSliderComponent,
+    NavbarComponent,
+    CustomHueComponent,
+    GradientCreatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    RgbSliderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
